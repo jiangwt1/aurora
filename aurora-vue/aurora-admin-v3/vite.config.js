789717cb660c28656650e8578e6e77af47f1eaa4
jiangwt1 <940658582@qq.com>
2026-05-01 11:52:00 +0800

    过滤告警

--- aurora-vue/aurora-admin-v3/vite.config.js
+++ aurora-vue/aurora-admin-v3/vite.config.js
@@ -92,6 +92,7 @@ export default defineConfig({
       preprocessorOptions: {
         scss: {
           api: 'modern-compiler',
+          silenceDeprecations: ['legacy-js-api'],
           additionalData: `@use "@/styles/variables.scss" as *;`
         }
       }
